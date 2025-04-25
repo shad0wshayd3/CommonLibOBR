cfe3cd5931d53347a421d2a19251f501a47a9284
powerof3 <32599957+powerof3@users.noreply.github.com>
2025-04-25 13:08:32 +0530

    Add IDs header

--- CommonLibOB64/include/RE/RTTI.h
+++ CommonLibOB64/include/RE/RTTI.h
@@ -108,7 +108,7 @@ namespace RE
 	inline void* RTDynamicCast(void* a_inptr, std::int32_t a_vfDelta, void* a_srcType, void* a_targetType, std::int32_t a_isReference)
 	{
 		using func_t = decltype(&RTDynamicCast);
-		REL::Relocation<func_t> func{ REL::Offset(0x6C23656) };
+		REL::Relocation<func_t> func{ ID::RTDynamicCast };
 		return func(a_inptr, a_vfDelta, a_srcType, a_targetType, a_isReference);
 	}
 
