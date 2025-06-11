8cb5178122218272398ff3e07e22d74c6e4b8e46
Qudix <17361645+Qudix@users.noreply.github.com>
2025-06-11 11:11:07 -0500

    feat: version

--- include/OBSE/Version.h
+++ include/OBSE/Version.h
@@ -5,6 +5,7 @@ namespace OBSE
 	inline constexpr OBSE::Version RUNTIME_0_411_140{ 0, 411, 140, 0 };
 	inline constexpr OBSE::Version RUNTIME_0_411_141{ 0, 411, 141, 0 };
 	inline constexpr OBSE::Version RUNTIME_0_411_142{ 0, 411, 142, 0 };
+	inline constexpr OBSE::Version RUNTIME_1_511_102{ 1, 511, 102, 0 };
 
-	inline constexpr auto RUNTIME_LATEST = RUNTIME_0_411_140;
+	inline constexpr auto RUNTIME_LATEST = RUNTIME_1_511_102;
 }
