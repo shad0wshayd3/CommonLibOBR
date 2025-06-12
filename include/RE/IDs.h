b3f1c714f6078d134a123ddb17385772b1ed49fb
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-06-12 09:29:00 -0600

    feat: function IDs

--- include/RE/IDs.h
+++ include/RE/IDs.h
@@ -4,203 +4,203 @@ namespace RE::ID
 {
 	namespace Actor
 	{
-		inline constexpr REL::Offset GetItemCountInContainer{ 0 };  // 0x65DA780
+		inline constexpr REL::ID GetItemCountInContainer{ 406091 };
 	}
 
 	namespace FORM_ENUM_STRING
 	{
-		inline constexpr REL::Offset FormEnumString{ 0x8F9FCE0 };
+		inline constexpr REL::ID FormEnumString{ 1462745 };
 	}
 
 	namespace GameSettingCollection
 	{
-		inline constexpr REL::Offset Singleton{ 0 };  // 0x8FE1E10
+		inline constexpr REL::ID Singleton{ 1464798 };
 	}
 
 	namespace InterfaceManager
 	{
-		inline constexpr REL::Offset GetInstance{ 0x65D7AB0 };
+		inline constexpr REL::ID GetInstance{ 406444 };
 	}
 
 	namespace MapMenu
 	{
-		inline constexpr REL::Offset CenterMapAt{ 0x655B990 };
-		inline constexpr REL::Offset SwitchTabs{ 0x6560E30 };
-		inline constexpr REL::Offset SwitchTabsNotifyingAltar{ 0x6560FD0 };
-		inline constexpr REL::Offset Update{ 0x6561220 };
+		inline constexpr REL::ID CenterMapAt{ 404937 };
+		inline constexpr REL::ID SwitchTabs{ 404990 };
+		inline constexpr REL::ID SwitchTabsNotifyingAltar{ 404991 };
+		inline constexpr REL::ID Update{ 404992 };
 	}
 
 	namespace MenuConsole
 	{
-		inline constexpr REL::Offset Instance{ 0x6630730 };
-		inline constexpr REL::Offset PrintLine{ 0x6630950 };
+		inline constexpr REL::ID Instance{ 407549 };
+		inline constexpr REL::ID PrintLine{ 407552 };
 	}
 
 	namespace PlayerCharacter
 	{
-		inline constexpr REL::Offset Singleton{ 0x9413B08 };
+		inline constexpr REL::ID Singleton{ 1667589 };
 	}
 
 	namespace Script
 	{
-		inline constexpr REL::Offset ParseParameters{ 0x662DCA0 };
+		inline constexpr REL::ID ParseParameters{ 407526 };
 	}
 
 	namespace SCRIPT_FUNCTION
 	{
-		inline constexpr REL::Offset ConsoleFunctions{ 0x8FB6600 };
-		inline constexpr REL::Offset ScriptFunctions{ 0x8FAF210 };
+		inline constexpr REL::ID ConsoleFunctions{ 1471272 };
+		inline constexpr REL::ID ScriptFunctions{ 1469048 };
 	}
 
 	namespace Setting
 	{
-		inline constexpr REL::Offset SetString{ 0 };  // 0x6710B70
+		inline constexpr REL::ID SetString{ 409216 };
 	}
 
 	namespace TESDataHandler
 	{
-		inline constexpr REL::Offset Singleton{ 0x9474418 };
+		inline constexpr REL::ID Singleton{ 1667969 };
 	}
 
 	namespace TESForm
 	{
-		inline constexpr REL::Offset AllForms{ 0x8FA0370 };
-		inline constexpr REL::Offset AllFormsByEditorID{ 0x8FA03B0 };
+		inline constexpr REL::ID AllForms{ 1462827 };
+		inline constexpr REL::ID AllFormsByEditorID{ 1462835 };
 	}
 
 	namespace TESFullName
 	{
-		inline constexpr REL::Offset GetFullName{ 0x657C4B0 };
+		inline constexpr REL::ID GetFullName{ 405447 };
 	}
 
 	namespace TESObjectREFR
 	{
-		inline constexpr REL::Offset GetInterior{ 0x657C740 };
-		inline constexpr REL::Offset GetWorldSpace{ 0x657E270 };
+		inline constexpr REL::ID GetInterior{ 405453 };
+		inline constexpr REL::ID GetWorldSpace{ 405508 };
 	}
 
 	namespace Tile
 	{
-		inline constexpr REL::Offset GetMenuByClass{ 0x66EF410 };
-		inline constexpr REL::Offset GetMenu{ 0x66EF3C0 };
+		inline constexpr REL::ID GetMenuByClass{ 409363 };
+		inline constexpr REL::ID GetMenu{ 409362 };
 	}
 
-	inline constexpr REL::Offset RTDynamicCast{ 0x6BF7A96 };
+	inline constexpr REL::ID RTDynamicCast{ 427658 };
 }
 
 namespace UE::ID
 {
 	namespace FFrame
 	{
-		inline constexpr REL::Offset KismetExecutionMessage{ 0 };  // 0x10FD030
+		inline constexpr REL::ID KismetExecutionMessage{ 116737 };
 	}
 
 	namespace FLogScopedCategoryAndVerbosityOverride
 	{
-		inline constexpr REL::Offset GetTLSCurrent{ 0 };  // 0x0EA74B0
+		inline constexpr REL::ID GetTLSCurrent{ 110003 };
 	}
 
 	namespace FMemory
 	{
-		inline constexpr REL::Offset Free{ 0x0E24570 };
-		inline constexpr REL::Offset Malloc{ 0x0E30FB0 };
-		inline constexpr REL::Offset Realloc{ 0 };       // 0x0E3EE30
-		inline constexpr REL::Offset QuantizeSize{ 0 };  // 0x0E3E800
+		inline constexpr REL::ID Free{ 108444 };
+		inline constexpr REL::ID Malloc{ 108612 };
+		inline constexpr REL::ID Realloc{ 108654 };
+		inline constexpr REL::ID QuantizeSize{ 108646 };
 	}
 
 	namespace FName
 	{
-		inline constexpr REL::Offset Ctor1{ 0x0F397B0 };
-		inline constexpr REL::Offset Ctor2{ 0x0F39920 };
-		inline constexpr REL::Offset AppendString{ 0x0F3F5C0 };
-		inline constexpr REL::Offset ToString{ 0x0F54EC0 };
+		inline constexpr REL::ID Ctor1{ 112049 };
+		inline constexpr REL::ID Ctor2{ 112051 };
+		inline constexpr REL::ID AppendString{ 112087 };
+		inline constexpr REL::ID ToString{ 112307 };
 	}
 
 	namespace FNamePool
 	{
-		inline constexpr REL::Offset GetSingleton{ 0x9061500 };
+		inline constexpr REL::ID GetSingleton{ 1478297 };
 	}
 
 	namespace FParse
 	{
-		inline constexpr REL::Offset Command{ 0 };  // 0x0F0F5E0
+		inline constexpr REL::ID Command{ 111232 };
 	}
 
 	namespace FSelfRegisteringExec
 	{
-		inline constexpr REL::Offset Ctor{ 0 };  // 0x0EEA960
-		inline constexpr REL::Offset Dtor{ 0 };  // 0x0EEAF70
+		inline constexpr REL::ID Ctor{ 110681 };
+		inline constexpr REL::ID Dtor{ 110692 };
 	}
 
 	namespace FString
 	{
-		inline constexpr REL::Offset AppendChar{ 0x0DDB660 };
-		inline constexpr REL::Offset AppendChars1{ 0x0DDB6E0 };
-		inline constexpr REL::Offset AppendChars2{ 0x0DDB6F0 };
-		inline constexpr REL::Offset Ctor1{ 0x0DD8E70 };
-		inline constexpr REL::Offset Ctor2{ 0x0DD9000 };
-		inline constexpr REL::Offset Reset{ 0x0DE4060 };
+		inline constexpr REL::ID AppendChar{ 107257 };
+		inline constexpr REL::ID AppendChars1{ 107258 };
+		inline constexpr REL::ID AppendChars2{ 107259 };
+		inline constexpr REL::ID Ctor1{ 107217 };
+		inline constexpr REL::ID Ctor2{ 107218 };
+		inline constexpr REL::ID Reset{ 107404 };
 	}
 
 	namespace FText
 	{
-		inline constexpr REL::Offset Ctor1{ 0 };     // 0x0E561F0
-		inline constexpr REL::Offset GetEmpty{ 0 };  // 0x0E6E7C0
-		inline constexpr REL::Offset Rebuild{ 0 };   // 0x0E7F220
+		inline constexpr REL::ID Ctor1{ 108973 };
+		inline constexpr REL::ID GetEmpty{ 109368 };
+		inline constexpr REL::ID Rebuild{ 109545 };
 	}
 
 	namespace FUObjectArray
 	{
-		inline constexpr REL::Offset GetSingleton{ 0x91077E0 };
+		inline constexpr REL::ID GetSingleton{ 1479488 };
 	}
 
 	namespace UAltarCheatManager
 	{
-		inline constexpr REL::Offset GetSelectedActor{ 0x2CDB4E0 };
-		inline constexpr REL::Offset SetSelectedActor{ 0x4771160 };
+		inline constexpr REL::ID GetSelectedActor{ 197301 };
+		inline constexpr REL::ID SetSelectedActor{ 304600 };
 	}
 
 	namespace UClass
 	{
-		inline constexpr REL::Offset FindFunctionByName{ 0 };  // 0x1005AA0
+		inline constexpr REL::ID FindFunctionByName{ 114200 };
 	}
 
 	namespace UEngine
 	{
-		inline constexpr REL::Offset GetSingleton{ 0x92734C0 };
-		inline constexpr REL::Offset GetCurrentPlayWorld{ 0x37E3EB0 };
+		inline constexpr REL::ID GetSingleton{ 1583525 };
+		inline constexpr REL::ID GetCurrentPlayWorld{ 241719 };
 	}
 
 	namespace UGameplayStatics
 	{
-		inline constexpr REL::Offset GetPlayerController{ 0x31AA560 };
+		inline constexpr REL::ID GetPlayerController{ 220839 };
 	}
 
 	namespace UObjectBaseUtility
 	{
-		inline constexpr REL::Offset GetFullName1{ 0 };  // 0x111F730
-		inline constexpr REL::Offset GetFullName2{ 0 };  // 0x111F8D0
+		inline constexpr REL::ID GetFullName1{ 117106 };
+		inline constexpr REL::ID GetFullName2{ 117108 };
 	}
 
 	namespace UWorld
 	{
-		inline constexpr REL::Offset GetSingleton{ 0x9275EF8 };
+		inline constexpr REL::ID GetSingleton{ 1584597 };
 	}
 
 	namespace VOblivionUEPairingGate
 	{
-		inline constexpr REL::Offset GetSingleton{ 0x4594D60 };
+		inline constexpr REL::ID GetSingleton{ 286331 };
 	}
 
-	inline constexpr REL::Offset AsyncTask{ 0x0DB8830 };
-	inline constexpr REL::Offset GetExecRegistry{ 0 };                 // 0x0EF7E60
-	inline constexpr REL::Offset GetExecRegistryLock{ 0 };             // 0x0EF7ED0
-	inline constexpr REL::Offset GetTransientPackage{ 0 };             // 0x109A510
-	inline constexpr REL::Offset IsInGameThread{ 0 };                  // 0x0E3C380
-	inline constexpr REL::Offset LowLevelFatalErrorHandler{ 0 };       // 0x0ED7980
-	inline constexpr REL::Offset StaticConstructObject_Internal{ 0 };  // 0x1129D30
-	inline constexpr REL::Offset StaticFindObjectFast{ 0 };            // 0x112C350
-	inline constexpr REL::Offset StaticFindObjectFastSafe{ 0 };        // 0x112CC40
-	inline constexpr REL::Offset StaticFindObject{ 0 };                // 0x112C0F0
-	inline constexpr REL::Offset StaticFindObjectSafe{ 0 };            // 0x112CCE0
+	inline constexpr REL::ID AsyncTask{ 106836 };
+	inline constexpr REL::ID GetExecRegistry{ 110855 };
+	inline constexpr REL::ID GetExecRegistryLock{ 110856 };
+	inline constexpr REL::ID GetTransientPackage{ 115232 };
+	inline constexpr REL::ID IsInGameThread{ 108577 };
+	inline constexpr REL::ID LowLevelFatalErrorHandler{ 110499 };
+	inline constexpr REL::ID StaticConstructObject_Internal{ 117210 };
+	inline constexpr REL::ID StaticFindObjectFast{ 117220 };
+	inline constexpr REL::ID StaticFindObjectFastSafe{ 117223 };
+	inline constexpr REL::ID StaticFindObject{ 117217 };
+	inline constexpr REL::ID StaticFindObjectSafe{ 117224 };
 }
